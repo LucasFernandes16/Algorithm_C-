fb506dbd5ae0237b868369681ca63c725c0e78de
Lucas Fernandes <68709936+LucasFernandes16@users.noreply.github.com>
2024-06-21 12:23:55 -0300

    Heap final

--- Workspace/Tree/Heap/main.cpp
+++ Workspace/Tree/Heap/main.cpp
@@ -1,115 +1,123 @@
 #include <iostream>
-#include <vector>
-#include<string>
+#include <string>
 
 using namespace std;
 
-
-class Heap{
-  private:
-    int *H;
+class Heap {
+private:
+    int* H;
     int n;
     int maxsize;
 
-    void create(int size,int array[]){
-      maxsize = size;
-      n = size;
-      H = array;
+    void create(int size, int array[]) {
+        maxsize = size;
+        n = size;
+        H = new int[maxsize + 1];
+        for (int i = 1; i <= n; i++) {
+            H[i] = array[i];
+        }
     }
 
-    void HeapBottomUp(int* H) {
-      for (int i = n / 2; i >= 1; i--) {
-        int k = i;
-        int v = H[k];
+    void HeapBottomUp() {
+        for (int i = n / 2; i >= 1; i--) {
+            int k = i;
+            int v = H[k];
+            bool heap = false;
+            while (!heap && (2 * k) <= n) {
+                int j = 2 * k; // posição do primeiro filho
+                if (j < n && H[j] < H[j + 1]) { // max heap
+                    j = j + 1; // encontra o maior filho
+                }
+                if (v >= H[j]) { // maxheap
+                    heap = true;
+                } else {
+                    H[k] = H[j];
+                    k = j;
+                }
+            }
+            H[k] = v;
+        }
+    }
+
+public:
+    Heap(int arr[], int c) {
+        create(c, arr);
+        HeapBottomUp();
+    }
+
+    ~Heap() {
+        delete[] H;
+    }
+
+    void printh() {
+        for (int i = 1; i <= n; i++) {
+            cout << H[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void printsort(int arr[]) {
+        for (int i = 1; i <= maxsize; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void add(int E) {
+        int v = E;
+        int k = n + 1;
         bool heap = false;
-        while (!heap && (2 * k) <= n) {
-          int j = 2 * k; // posição do primeiro filho
-          if (j < n && H[j] > H[j + 1]) {// max heap
-            j = j + 1; // encontra o maior filho
-          }
-          if (v <= H[j]) {// maxheap
-            heap = true;
-          } else {
-            H[k] = H[j];
+
+        while (!heap && k > 1) {
+            int j = k / 2;
+            if (H[j] > v) { // maxheap
+                H[k] = H[j];
+            } else {
+                H[k] = v;
+                heap = true;
+            }
             k = j;
-          }
         }
-        H[k] = v;
-      }
-    }
-
-  public:
-    Heap(int arr[], int c){
-      create(c,arr);
-      HeapBottomUp(arr);
-    }
-    ~Heap(){}
-
-    void printh(){
-      for(int i = 1; i <= n; i++){
-        cout << H[i] << " ";
-      }
-    }
-    void printsort(int arr[]){
-      for(int i = 1; i <= maxsize; i++){
-        cout << arr[i] << " ";
-      }
-    }
-
-    void add(int E){
-      int v = E;
-      int k = n+1;
-      bool heap = false;
-
-      while (heap != true && k >= 1){
-        int j = k/2;
-        if(H[j] > v){// maxheap
-          H[k] = H[j];
+        if (!heap) {
+            H[k] = v;
         }
-        else{
-          H[k] = v;
-          heap = true;
+        n++;
+    }
+
+    void heapsort() {
+        int temp[maxsize + 1];
+        int original_n = n;
+        int i = 1;
+
+        while (n >= 1) {
+            temp[i] = H[1];
+            H[1] = H[n];
+            n--;
+            HeapBottomUp();
+            i++;
         }
-        k = j;
-      }
-      n++;
+
+        n = original_n;
+        for (int i = 1; i <= original_n; i++) {
+            H[i] = temp[i];
+        }
+
+        printsort(temp);
     }
-  
-  void heapsort(){
-    int temp[n+1];
-    int i = 1;
-    while(n >= 1){
-      int v = H[n];
-      temp[i] = H[n] = H[1];
-      H[1] = v;
-      i++;
-      n--;
-      HeapBottomUp(H);
-      printh();
-      cout << endl;
-    }
-    printsort(temp);
-  }
-  
 };
 
-int main(){
-  int arr[12];
-  int c,n;
-  cin >> c;
-  for(int i = 1; i <= c; i++){
-    cin >> n;
-    arr[i] = n;
-  }
+int main() {
+    int arr[12];
+    int c, n;
+    cin >> c;
+    for (int i = 1; i <= c; i++) {
+        cin >> n;
+        arr[i] = n;
+    }
 
-  Heap lucas(arr,c);
-  lucas.printh();
-  cout << endl;
-//  lucas.add(12);
-//  lucas.printh();
-//  cout << endl;
-  lucas.heapsort();
-  return 0;
+    Heap heap(arr, c);
+    heap.printh();
+    cout << endl;
+    heap.heapsort();
+    return 0;
 }
-
-
