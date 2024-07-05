97c5d191ecb77f6b1565abd4839705a44ed8b377
Lucas Fernandes <68709936+LucasFernandes16@users.noreply.github.com>
2024-07-05 13:43:07 -0300

    A

--- Workspace/Lista4/A/main.cpp
+++ Workspace/Lista4/A/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< Updated upstream
 #include <algorithm>
 
 using namespace std;
@@ -7,11 +8,21 @@ template <typename Key, typename E>
 class BSTNODE {
 public:
     int size;
+=======
+#include <string>
+
+using namespace std;
+
+template <typename Key,typename E>
+class BSTNODE{
+  public:
+>>>>>>> Stashed changes
     int height;
     Key key;
     BSTNODE* left;
     BSTNODE* right;
     E element;
+<<<<<<< Updated upstream
 };
 
 template <typename Key, typename E>
@@ -175,3 +186,147 @@ int main() {
     }
     return 0;
 }
+=======
+    
+};
+
+template <typename Key,typename E>
+class BST{
+  private:
+    BSTNODE<Key,E>* root;
+    int nodecount;
+    int cont;
+    bool found;
+
+    void create_bst(){
+      root = NULL;
+      nodecount = 0;
+    }
+    void clear_bst(BSTNODE<Key,E>* rt){
+      nodecount = 0;
+    }
+
+    BSTNODE<Key,E>* create_bstnode(Key k, E e){
+      BSTNODE<Key,E>* temp = new BSTNODE<Key,E>;
+      temp->key = k;
+      temp->element = e;
+      temp->left = temp->right = NULL;
+      temp->height = 0;
+      return temp;
+    }
+
+    int h(BSTNODE<Key,E>* rt){
+      if(rt == NULL){return -1;}
+      return rt->height;
+    }
+
+    int getBalance(BSTNODE<Key,E>* rt){
+      if(rt == NULL){return 0;}
+      return h(rt->left) - h(rt->right);
+    }
+
+    BSTNODE<Key,E>* inserthelp(BSTNODE<Key,E>* rt, Key k, E e){
+      if(rt == NULL){return create_bstnode(k,e);}
+      if(rt->key > k){
+        rt->left = inserthelp(rt->left, k, e);
+      }
+      else{
+        rt->right = inserthelp(rt->right, k ,e);
+      }
+      rt->height = 1 + max(h(rt->left),h(rt->right));
+      int balance = getBalance(rt);
+      if(balance < -1 && k >= rt->right->key){return leftRotate(rt);}
+      if(balance > 1 && k < rt->left->key){return rightRotate(rt);}
+      if (balance > 1 && k >= rt->left->key){
+        rt->left = leftRotate(rt->left);
+        return rightRotate(rt);
+      }
+      if(balance < -1 && k < rt->right->key){
+        rt->right = rightRotate(rt->right);
+        return leftRotate(rt);
+      }
+      return rt;
+    }
+    
+    BSTNODE<Key,E>* rightRotate(BSTNODE<Key,E>* rt){
+      BSTNODE<Key,E>* l = new BSTNODE<Key,E>;
+      BSTNODE<Key,E>* lr = new BSTNODE<Key,E>;
+      l = rt->left;
+      lr = l->right;
+      l->right = rt;
+      rt->left = lr;
+      rt->height = max(h(rt->left),h(rt->right)) + 1;
+      l->height = max(h(l->left),h(l->right)) + 1;
+      return l;
+    }
+
+    BSTNODE<Key,E>* leftRotate(BSTNODE<Key,E>* rt){
+      BSTNODE<Key,E>* r = new BSTNODE<Key,E>;
+      BSTNODE<Key,E>* rl = new BSTNODE<Key,E>;
+      r = rt->right;
+      rl = r->left;
+      r->left = rt;
+      rt->right = rl;
+      rt->height = max(h(rt->left),h(rt->right)) + 1;
+      r->height = max(h(r->left),h(r->right)) + 1;
+      return r;
+    }
+
+  Key inorder(BSTNODE<Key,E>* rt, Key k){
+    if(rt != NULL && found != true){
+      inorder(rt->left,k);
+      if(rt->key == k){
+        found = true;
+        cont ++;
+      }else if(found != true){
+        cont++;
+      }
+      inorder(rt->right, k);
+    }
+    return cont;
+  }
+
+
+  public:
+    BST(){create_bst();}
+
+    void insert(Key k, E e){
+      root = inserthelp(root, k, e);
+      nodecount++;
+    }
+
+    E find(Key k){
+        found = false;
+        cont = 0;
+        int c = inorder(root,k);
+        if(found == false){
+          return 0;
+        }else{
+          return c;
+        }
+    }
+};
+
+int main(){
+  BST<int,int> avl;
+  int q,x,op,pos;
+  cin >> q;
+  for(int i = 0; i < q; i++){
+    cin >> op >> x;
+    if(op == 1){
+      avl.insert(x,0);
+    }else{
+      pos = avl.find(x);
+      if(pos == 0){
+        cout << "Data tidak ada";
+      }else{
+        cout << pos;
+      }
+      if(i < q){
+        cout << endl;
+      }
+    }
+  }
+  return 0;
+}
+>>>>>>> Stashed changes
